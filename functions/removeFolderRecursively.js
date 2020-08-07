aec9082c8feca0e1174a9f739d63ed628053cff7
Esteban Cazorla Persson <ecazorla@gmail.com>
2020-08-08 00:55:52 +0200

    CREATE removeFolder function

---
+++ functions/removeFolderRecursively.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const Path = require('path');
+
+const deleteFolderRecursive = (path, removeParent = false) => {
+	if (fs.existsSync(path)) {
+		fs.readdirSync(path).forEach((file) => {
+			const curPath = Path.join(path, file);
+
+			if (fs.lstatSync(curPath).isDirectory()) {
+				deleteFolderRecursive(curPath, true);
+			} else {
+				fs.unlinkSync(curPath);
+			}
+		});
+
+		if (removeParent === true) {
+			fs.rmdirSync(path);
+		}
+	}
+};
+
+module.exports = deleteFolderRecursive;
