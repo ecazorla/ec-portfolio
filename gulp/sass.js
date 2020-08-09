3a5f408b781afd262d863b0f0893bd9db0098a00
Esteban Cazorla Persson <ecazorla@gmail.com>
2020-08-09 21:14:06 +0200

    UPDATE sass gulp task with option compressed

--- gulp/sass.js
+++ gulp/sass.js
@@ -2,7 +2,9 @@ const compileScss = (Gulp, Sass, NodeSass) => (cb) => {
 	Sass.compiler = NodeSass;
 
 	Gulp.src('./src/scss/**/*.scss')
-		.pipe(Sass().on('error', Sass.logError))
+		.pipe(Sass({
+			outputStyle: 'compressed'
+		}).on('error', Sass.logError))
 		.pipe(Gulp.dest('./site'));
 	cb();
 };
