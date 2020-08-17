e7f457787ebc2ba57b994068c4940f5c6c403ac8
Esteban Cazorla Persson <ecazorla@gmail.com>
2020-08-17 11:46:09 +0200

    ADD partials to handlebars compiling process

--- gulp/handlebars.js
+++ gulp/handlebars.js
@@ -2,27 +2,35 @@
 
 const templatePath = './src/templates/page.hbs';
 const pagesFolder = './content/pages';
+const partialsFolder = './src/partials';
 const siteFolder = './site';
 
-const handlebars = (fs, Handlebars) => (cb) => {
+const handlebars = (fs, Handlebars) => async (cb) => {
 	if (!fs.existsSync(siteFolder)) {
 		fs.mkdirSync(siteFolder);
 	}
 
-	fs.readdir(pagesFolder, (err, pages) => {
-		if (err) {
-			throw new Error(err);
-		}
+	// Register partials
+	const partialsFiles = fs.readdirSync(partialsFolder);
+	await Promise.all(partialsFiles.map((partialName) => {
+		console.log(`Handlebars.js: registering  partial ${partialName}`);
+		const partialNameNoExtension = partialName.split('.')[0];
+		const partialPath = `${partialsFolder}/${partialName}`;
+		const partialData = fs.readFileSync(partialPath).toString();
+		Handlebars.registerPartial(partialNameNoExtension, partialData);
+	}));
 
-		pages.forEach(page => {
-			const pageData = JSON.parse(fs.readFileSync(`${pagesFolder}/${page}`).toString());
-			const pageTemplateString = fs.readFileSync(templatePath).toString();
-			const pageTemplateCompiled = Handlebars.compile(pageTemplateString);
-			const pageRendered = pageTemplateCompiled(pageData);
+	// Compiling pages
+	const pagesFiles = fs.readdirSync(pagesFolder);
+	await Promise.all(pagesFiles.map((pageName) => {
+		console.log(`Handlebars.js: compiling page ${pageName}`);
+		const pageData = JSON.parse(fs.readFileSync(`${pagesFolder}/${pageName}`).toString());
+		const pageTemplateString = fs.readFileSync(templatePath).toString();
+		const pageTemplateCompiled = Handlebars.compile(pageTemplateString);
+		const pageRendered = pageTemplateCompiled(pageData);
 
-			fs.writeFileSync(`${siteFolder}/${pageData.fields.url}.html`, pageRendered);
-		});
-	});
+		fs.writeFileSync(`${siteFolder}/${pageData.fields.url}.html`, pageRendered);
+	}));
 
 	cb();
 };
